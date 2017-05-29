52f4a057a9ce936a3e2c18d5175f5eec8ae45628
zhukovsd <zhukovsd@gmail.com>
2017-05-29 19:43:07 +0300

    Watch html and css files by using "cp -u"

--- watch.sh
+++ watch.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 watchify app/app.js -t [babelify app] --extension=.jsx --debug -o build/app.js -v &
-browser-sync start --server build --files build/app.js --port 3000 --reload-delay 100 &
+
+while true; do cp -u ./app/*.html ./app/*.css ./build/; sleep 2; done &
+
+#browser-sync start --server build --files build/app.js --port 3000 --reload-delay 100 &
+browser-sync start --server build --files build/* --port 3000 --reload-delay 100 &
 
 wait
