1eeaab7351e55b6f9415d7c07a7d1101ebfe26ea
zhukovsd <zhukovsd@gmail.com>
2017-05-30 02:49:14 +0300

    Button with a link to github project in the appbar

--- app/components/App/index.jsx
+++ app/components/App/index.jsx
@@ -5,6 +5,8 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+
 import Calc from "./Calc";
 
 export default class App extends React.Component {
@@ -12,7 +14,16 @@ export default class App extends React.Component {
         return (
             <MuiThemeProvider>
                 <div className="app">
-                    <AppBar title="FreeCodeCamp calculator"/>
+                    <AppBar
+                        title="FreeCodeCamp calculator"
+                        iconElementRight={
+                            <IconButton
+                                iconClassName="muidocs-icon-custom-github"
+                                linkButton={true} target="_blank"
+                                href="https://github.com/zhukovsd/freecodecamp-calculator"
+                            />
+                        }
+                    />
                     <Calc/>
                 </div>
             </MuiThemeProvider>
